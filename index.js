1656908edfa25ba1b118e666dc3bfb9c2910c180
Rupesh Sahu <99035170+Rupesh2806@users.noreply.github.com>
2023-06-20 12:15:46 +0530

    Update index.py

--- index.js
+++ index.js
@@ -1,67 +1,53 @@
-const MORSE_CODE = {
-    '.-': 'A',
-    '-...': 'B',
-    '-.-.': 'C',
-    '-..': 'D',
-    '.': 'E',
-    '..-.': 'F',
-    '--.': 'G',
-    '....': 'H',
-    '..': 'I',
-    '.---': 'J',
-    '-.-': 'K',
-    '.-..': 'L',
-    '--': 'M',
-    '-.': 'N',
-    '---': 'O',
-    '.--.': 'P',
-    '--.-': 'Q',
-    '.-.': 'R',
-    '...': 'S',
-    '-': 'T',
-    '..-': 'U',
-    '...-': 'V',
-    '.--': 'W',
-    '-..-': 'X',
-    '-.--': 'Y',
-    '--..': 'Z',
-    '-----': '0',
-    '.----': '1',
-    '..---': '2',
-    '...--': '3',
-    '....-': '4',
-    '.....': '5',
-    '-....': '6',
-    '--...': '7',
-    '---..': '8',
-    '----.': '9',
-    '.-.-.-': '.',
-    '--..--': ',',
-    '..--..': '?',
-    '.----.': "'",
-    '-.-.--': '!',
-    '-..-.': '/',
-    '-.--.': '(',
-    '-.--.-': ')',
-    '.-...': '&',
-    '---...': ':',
-    '-.-.-.': ';',
-    '-...-': '=',
-    '.-.-.': '+',
-    '-....-': '-',
-    '..--.-': '_',
-    '.-..-.': '"',
-    '...-..-': '$',
-    '.--.-.': '@',
-    '...---...': 'SOS'
+MORSE_CODE = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+    '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+    '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+    '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+    '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+    '--..': 'Z', '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9',
+    '-----': '0', '--..--': ',', '.-.-.-': '.', '..--..': '?', '-..-.': '/',
+    '-....-': '-', '-.--.': '(', '-.--.-': ')', '.-...': '&', '---...': ':',
+    '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_',
+    '.-..-.': '"', '...-..-': '$', '.--.-.': '@', '...---...': 'SOS'
 }
 
+def decodeBits(bits):
+    # Determine the transmission rate
+    min_time_unit = float('inf')
+    i = 0
+    while i < len(bits):
+        if bits[i] == '1':
+            j = i + 1
+            while j < len(bits) and bits[j] == '0':
+                j += 1
+            if j - i < min_time_unit:
+                min_time_unit = j - i
+            i = j
+        else:
+            i += 1
 
-const decodeMorse = (morseCode) => {
-    return morseCode
-}
-  
-  
-const decodeBits = (bits) => {
-    return bits
-}
+    time_unit = min_time_unit // 1  # Round down to the nearest integer
+
+    # Decode the message
+    morse_code = []
+    bit_groups = bits.strip('0').split('0' * time_unit)
+    for group in bit_groups:
+        if group == '':
+            morse_code.append(' ')
+        else:
+            symbol = '.' * (group.count('1') // time_unit)
+            morse_code.append(symbol)
+
+    return ''.join(morse_code)
+
+def decodeMorse(morseCode):
+    # Convert Morse code to human-readable string
+    morse_words = morseCode.strip().split('   ')
+    decoded_message = []
+    for word in morse_words:
+        morse_letters = word.split(' ')
+        decoded_letters = [MORSE_CODE[letter] for letter in morse_letters if letter in MORSE_CODE]
+        decoded_message.append(''.join(decoded_letters))
+
+    return ' '.join(decoded_message)
